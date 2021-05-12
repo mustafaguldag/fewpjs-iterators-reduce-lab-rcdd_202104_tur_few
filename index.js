376e4adde72027bd6411c2e3ac1d28230dfa6a9d
mustafaguldag <guldag.mustafaa@gmail.com>
2021-05-12 17:40:13 +0300

    commit tree 0bb86679ecb7dcbba9b29d1e9ba226e219bf103c

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+// function totalBatteries(batteryBatches){
+//     let result = 0;
+    
+//     return batteryBatches.reduce(total, currentValue => {
+//         result = total + currentValue
+
+//         return result;
+//     },0);
+    
+    
+// }
+// totalBatteries(batteryBatches);
+
+
+
+let totalBatteries = batteryBatches.reduce( (total, currentValue) => {
+    let result = 0;
+    result = total + currentValue
+    return result;
+},0);
+    
+    
+    
+
+
